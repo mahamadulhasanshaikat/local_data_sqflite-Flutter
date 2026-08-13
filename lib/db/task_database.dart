7b15fa092c05286a36caa152c6bb6f5bfa3bfd49
md-mahamadul-hasan-shaikat <113624647+md-mahamadul-hasan-shaikat@users.noreply.github.com>
2026-08-14 02:15:04 +0600

    update databse code

---
+++ lib/db/task_database.dart
@@ -0,0 +1,9 @@
+import 'package:sqflite/sqflite.dart';
+
+class TaskDatabase {
+  static Database? db;
+
+  static Future<Database>getDB(){
+    
+  }
+}
--- lib/model/task.dart
+++ lib/model/task.dart
@@ -10,6 +10,8 @@ class Task {
   }
 
   factory Task.formMap(Map<String, dynamic> map) {
-    return Task(title: map['title'], isDone: map['isDone'] == 1);
+    return Task(id: map['id'], title: map['title'], isDone: map['isDone'] == 1);
   }
+
+  
 }
