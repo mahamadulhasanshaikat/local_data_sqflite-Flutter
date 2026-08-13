72bf6fa777ca2e0a9919b6de603454bde2cb3bd3
md-mahamadul-hasan-shaikat <113624647+md-mahamadul-hasan-shaikat@users.noreply.github.com>
2026-08-13 23:26:15 +0600

    update

--- lib/view/task_home_page.dart
+++ lib/view/task_home_page.dart
@@ -10,6 +10,32 @@ class TaskHomePage extends StatefulWidget {
 class _TaskHomePageState extends State<TaskHomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text('Todo List'), backgroundColor: Colors.orange),
+
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: TextFormField(
+                  decoration: InputDecoration(hintText: 'Enter a task'),
+                ),
+              ),
+              IconButton(icon: Icon(Icons.add), onPressed: () {}),
+            ],
+          ),
+
+          ListView.builder(
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return ListTile(
+                leading: Checkbox(value: false, onChanged:(_){}),
+              );
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
